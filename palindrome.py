5331a662472ad6a2300066f02ccc810cd30d8361
alexandrebeugnies <beugniesalexandre@outlook.com>
2023-06-19 14:52:38 +0200

    fin Palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,12 @@
 class Palindrome:
+    def __init__(self,chaine):
+        self.chaine = chaine
+
+    def __del__(self):
+        print(self.chaine.upper())
+
+    def test(self):
+        return Palindrome.estPalindrome(self.chaine)
     # Méthode de classe testant si une chaine de Caracteres s est un palindrome
     def estPalindrome(s):
         # Une chaine d'un seul caractere ou une chaine vide sont des Palindromes
@@ -7,3 +15,9 @@ class Palindrome:
         # Si les premiers et derniers caracteres sont identiques
         # et si la sous-chaine restante est elle-meme
         # un palindrome, alors la chaine entiere est un palindrome
+
+        # l´utilisation de l´indice -1 permet de recupere le dernier caractere d'une chaine
+        # l'utilisation du caractere ':' permet d'extraire une sous-chaine en precisant les indices de debut et de fin.
+        return s[0] == s[-1] and Palindrome.estPalindrome(s[1:-1])
+
+        
